9120952f3deec6fdb149accc19b1510f864362f6
danielvieiralopes <danielvieira.2007@hotmail.com>
2020-07-13 16:40:51 -0300

    savepoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
+import 'package:contatos/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    
+    debugShowCheckedModeBanner: false,
+    home: HomePage(),
   ));
 }
